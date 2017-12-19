cc9b82635750d9809f2e23393484b40c75a797ff
r4wm <raymondmintz11@gmail.com>
2017-12-19 15:46:30 -0800

    saving

--- lua-ectoken/ectoken.lua
+++ lua-ectoken/ectoken.lua
@@ -143,19 +143,7 @@ end
 -- ----
 -- main
 -- ----
--- getopt, POSIX style command line argument parser
--- param arg contains the command line arguments in a standard table.
--- param options is a string with the letters that expect string values.
--- returns a table where associated keys are true, nil, or a string value.
--- The following example styles are supported
---   -a one  ==> opts["a"]=="one"
---   -bone   ==> opts["b"]=="one"
---   -c      ==> opts["c"]==true
---   --c=one ==> opts["c"]=="one"
---   -cdaone ==> opts["c"]==true opts["d"]==true opts["a"]=="one"
--- note POSIX demands the parser ends at the first non option
---      this behavior isn't implemented.
--- function taken from public domain at http://lua-users.org/wiki/AlternativeGetOpt
+-- getopt alternative function taken from public domain at http://lua-users.org/wiki/AlternativeGetOpt
 function getopt( arg, options )
   local tab = {}
   for k, v in ipairs(arg) do
